43a7f24aa9208dcf2e729454040ecb7549857d10
ramkshukla <ramkumarshukla00000@gmail.com>
2025-04-10 12:59:37 +0530

    code updated

--- src/main/java/com/foodordersystem/FoodOrder.java
+++ src/main/java/com/foodordersystem/FoodOrder.java
@@ -13,29 +13,27 @@ public class FoodOrder {
     static FoodOrderService service = new FoodOrderServiceImpl();
 
     public static void main(String[] args) {
-        User user1 = new User("Ashwin", 500.0);
-        User user2 = new User("Harish", 300.0);
-        User user3 = new User("Shruthi", 700.0);
-        User user4 = new User("Diya", 200.0);
+        User ashwin = new User("Ashwin", 500.0);
+        User harish = new User("Harish", 300.0);
+        User shruthi = new User("Shruthi", 700.0);
+        User diya = new User("Diya", 200.0);
 
-        service.createUser(user1);
-        service.createUser(user2);
-        service.createUser(user3);
-        service.createUser(user4);
+        service.createUser(ashwin);
+        service.createUser(harish);
+        service.createUser(shruthi);
+        service.createUser(diya);
 
-        service.addToUserWallet("Ashwin", 500.0);
-        service.addToUserWallet("Harish", 300.0);
-        service.addToUserWallet("Shruthi", 700.0);
-        service.addToUserWallet("Diya", 200.0);
+        service.addToUserWallet(ashwin.getName(), 500.0);
+        service.addToUserWallet(harish.getName(), 300.0);
+        service.addToUserWallet(shruthi.getName(), 700.0);
+        service.addToUserWallet(diya.getName(), 200.0);
 
-        System.out.println("==== Onboarding Restaurants ====");
 
         Restaurant r1 = new Restaurant("R1", 4.5, 5);
         r1.addMenuItem("Veg Biryani", 100);
         r1.addMenuItem("Chicken Biryani", 150);
         r1.addMenuItem("Dosa", 60);
         service.onboardRestaurant(r1);
-        System.out.println("Added: " + r1.getName());
 
         Restaurant r2 = new Restaurant("R2", 4.0, 5);
         r2.addMenuItem("Idli", 10);
@@ -43,7 +41,6 @@ public class FoodOrder {
         r2.addMenuItem("Veg Biryani", 80);
         r2.addMenuItem("Chicken Biryani", 175);
         service.onboardRestaurant(r2);
-        System.out.println("Added: " + r2.getName());
 
         Restaurant r3 = new Restaurant("R3", 4.9, 1);
         r3.addMenuItem("Idli", 15);
@@ -51,38 +48,30 @@ public class FoodOrder {
         r3.addMenuItem("Gobi Manchurian", 150);
         r3.addMenuItem("Chicken Biryani", 175);
         service.onboardRestaurant(r3);
-        System.out.println("Added: " + r3.getName());
 
-        System.out.println("\n==== Updating Restaurant Menus ====");
 
         r1.addMenuItem("Chicken65", 250);
-        System.out.println("Menu updated for R1: Added Chicken65");
 
         service.updateMenu("R2", "Chicken Biryani", 150);
-        System.out.println("Menu updated for R2: Updated price of Chicken Biryani");
 
-        System.out.println("\n==== Placing Orders ====");
 
-        Order order1 = new Order("Ashwin", Map.of("Idli", 3, "Dosa", 1), SelectionStrategyType.LOWEST_COST);
+        Order order1 = new Order(ashwin.getName(), Map.of("Idli", 3, "Dosa", 1), SelectionStrategyType.LOWEST_COST);
         System.out.println(service.placeOrder(order1));
 
-        Order order2 = new Order("Harish", Map.of("Idli", 3, "Dosa", 1), SelectionStrategyType.LOWEST_COST);
+        Order order2 = new Order(harish.getName(), Map.of("Idli", 3, "Dosa", 1), SelectionStrategyType.LOWEST_COST);
         System.out.println(service.placeOrder(order2));
 
-        Order order3 = new Order("Shruthi", Map.of("Veg Biryani", 3, "Dosa", 1), SelectionStrategyType.HIGHEST_RATING);
+        Order order3 = new Order(shruthi.getName(), Map.of("Veg Biryani", 3, "Dosa", 1), SelectionStrategyType.HIGHEST_RATING);
         System.out.println(service.placeOrder(order3));
 
-        System.out.println("\n==== Updating Orders ====");
         service.markOrderAsCompleted("R3");
-        System.out.println("R3 marks Order 01 as COMPLETED");
 
-        System.out.println("\n==== Placing More Orders ====");
 
-        Order order4 = new Order("Harish", Map.of("Idli", 3, "Dosa", 1), SelectionStrategyType.LOWEST_COST);
+        Order order4 = new Order(harish.getName(), Map.of("Idli", 3, "Dosa", 1), SelectionStrategyType.LOWEST_COST);
         System.out.println(service.placeOrder(order4));
 
 
-        Order order5 = new Order("Diya", Map.of("Idli", 3, "Paneer Tikka", 1), SelectionStrategyType.LOWEST_COST);
-        System.out.println(service.placeOrder(order5));
+        Order order5 = new Order(diya.getName(), Map.of("Idli", 3, "Paneer Tikka", 1), SelectionStrategyType.LOWEST_COST);
+        System.out.println(service.placeOrder(order5)); 
 }
 }
--- src/main/java/com/foodordersystem/service/FoodOrderService.java
+++ src/main/java/com/foodordersystem/service/FoodOrderService.java
@@ -6,14 +6,9 @@ import main.java.com.foodordersystem.model.User;
 
 public interface FoodOrderService {
     void onboardRestaurant(Restaurant restaurant);
-
     void updateMenu(String restaurantName, String item, int price);
-
     String placeOrder(Order order);
-
     void markOrderAsCompleted(String restaurantName);
-
     void createUser(User user);
-
     void addToUserWallet(String userName, double amount);
 }
